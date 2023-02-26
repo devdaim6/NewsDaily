ca5033793b0511a295cb196f2303e93d12362308
devdaim6 <daimdev6@gmail.com>
2023-02-26 21:45:09 +0530

    apikey changed to newsapi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import News from "./components/News";
-import Navbar from "./Navbar";
 import React from "react";
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+process.env.CI = false;
 root.render(
   <React.StrictMode>
     <App />
